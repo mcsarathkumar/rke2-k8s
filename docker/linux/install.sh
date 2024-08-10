f82561a8f12a77b871f966f6029031a367fe5fd8
SarathKumar <sarathkumar0510@gmail.com>
2024-08-11 01:51:03 +0530

    rke2 config set up

---
+++ docker/linux/install.sh
@@ -0,0 +1,3 @@
+curl -fsSL https://get.docker.com -o install-docker.sh
+sh install-docker.sh
+usermod -aG docker $USER
---
+++ rke2-setup/cluster/main.sh
@@ -0,0 +1,28 @@
+# Install RKE2
+curl -sfL https://get.rke2.io | sh
+
+# Configure RKE2
+mkdir -p /etc/rancher/rke2
+cp ./config.yaml /etc/rancher/rke2/config.yaml
+
+# Start Services
+systemctl enable rke2-server.service
+systemctl start rke2-server.service
+
+echo $PATH
+
+# Set Environment Variables
+echo PATH="$PATH:/var/lib/rancher/rke2/bin/" > /etc/environment
+echo KUBECONFIG=/etc/rancher/rke2/rke2.yaml >> /etc/environment
+echo alias k='kubectl' >> /etc/environment
+
+source /etc/environment
+
+# Install Helm
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# Node Config yaml
+PUBLIC_IP=$(curl -s ifconfig.me)
+NODE_TOKEN=$(cat /var/lib/rancher/rke2/server/node-token)
+echo server: https://$PUBLIC_IP:9345 > node-config.yaml
+echo token: $NODE_TOKEN >> node-config.yaml
